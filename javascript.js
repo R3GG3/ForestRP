62d94b424f9e1aa41a478c48b5ad1b1c1ced3dde
R3GG3 <konrad.soczi@gmail.com>
2019-07-12 20:14:49 +0200

    Commit Made By Auto Git from R3GG3 and HiGh_SkUlL!

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function onas()
+{
+    $('#content').delay(1000).fadeIn(1000, function(){});
+    $('#content2').fadeOut(1000, function(){});
+}
+
+function kontakt()
+{
+    $('#content').fadeOut(1000, function(){});
+    
+    $('#content2').delay(1000).fadeIn(1000, function(){});
+}
