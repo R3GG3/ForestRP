68c25e0a6504fbdc6aee18e125bb1d079b320209
R3GG3 <konrad.soczi@gmail.com>
2019-07-13 21:57:18 +0200

    Added Mobile Version - Not completed

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+window.onload = start;
+
+function start()
+{
+    $('#mySidenav').css('width', '0')
+    nav();
+}
+
+
 function onas()
 {
     $('#content').delay(1000).fadeIn(1000, function(){});
@@ -9,4 +18,37 @@ function kontakt()
     $('#content').fadeOut(1000, function(){});
     
     $('#content2').delay(1000).fadeIn(1000, function(){});
+}
+
+function openNav()
+{
+    $( "#nav" ).animate({
+    width: "toggle"
+  }, 1000, function() {});
+    document.getElementById("opennavbutton").style.display = "none";
+}
+
+function closeNav()
+{
+    $( "#nav" ).animate({
+        width: "toggle"
+      }, 1000, function() {});
+    $('#opennavbutton').delay(1000).fadeIn(1000, function(){});
+}
+
+function nav()
+{
+    var windowwidth = $(window).width();
+
+    if(windowwidth <= 1360)
+    {
+        
+    }
+
+    if(windowwidth > 1360)
+    {
+        
+    }
+
+    setTimeout("nav()", 1);
 }
